5e33fbc0c799c7aa894841b1ea2d65e400766b6e
notpop <k.tani.free@gmail.com>
2020-11-29 03:03:50 +0900

    環境変数取得完了

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,10 +1,6 @@
 import axios from "axios"
 
 const KEY = process.env.REACT_APP_YOUTUBE_DATA_V3_API_KEY;
-console.log('api_key');
-console.log(KEY);
-console.log(process.env);
-console.log('api_key_end');
 
 const youtube = axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3"
